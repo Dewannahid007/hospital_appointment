6219a8be1d578af6c241bb906c2069eb73fd9bbc
Dewannahid007 <islam35-1715@diu.edu.bd>
2022-09-29 01:12:51 +0600

    hospital appointment created

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.main')
+@section('content')
+ <div class="container-lg" style="margin: 0 auto ;">
+ <div class="row mt-5">
+
+
+    <div class="col-lg-4 col-md4 col-sm-12 text-center mb-3  "> 
+        <div class="card" style="width: 18rem;">
+        <img src="" alt="">
+        <div class="card-body">
+            <div class="card-title">title</div>
+            <div class="card-text">content</div>
+        </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md4 col-sm-12 text-center mb-3  "> 
+        <div class="card" style="width: 18rem;">
+        <img src="" alt="">
+        <div class="card-body">
+            <div class="card-title">title</div>
+            <div class="card-text">content</div>
+        </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md4 col-sm-12 text-center mb-3  "> 
+        <div class="card" style="width: 18rem;">
+        <img src="" alt="">
+        <div class="card-body">
+            <div class="card-title">title</div>
+            <div class="card-text">content</div>
+        </div>
+        </div>
+    </div>
+
+
+ </div>
+</div>
+
+
+
+
+@endsection
